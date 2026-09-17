0e5ab3a59ab9c99938987975a5c7d0267765431a
Anshul-Hooda <anshul22032007@gmail.com>
2026-09-18 01:51:51 +0530

    Fix indentation for loading contract ABI

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ if RPC_URL and PRIVATE_KEY and CONTRACT_ADDRESS:
 
     CONTRACT_ADDRESS = Web3.to_checksum_address(CONTRACT_ADDRESS)
 
-   with open("cityproofs_abi.json", "r") as f:
-    CONTRACT_ABI = json.load(f)
+    with open("cityproofs_abi.json", "r") as f:
+        CONTRACT_ABI = json.load(f)
 
     contract = web3.eth.contract(
         address=CONTRACT_ADDRESS,
