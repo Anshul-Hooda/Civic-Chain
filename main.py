616d1d67548a496e43507a29f8f7aabab30bdb8f
Anshul-Hooda <anshul22032007@gmail.com>
2026-09-18 00:07:38 +0530

    Integrate Web3 for Ethereum contract management
    Added Web3 configuration and contract setup for Ethereum interaction.

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from blockchain import blockchain
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
+from web3 import Web3
+import hashlib
+import json
+import os
+
 import models
 import schemas
 
@@ -74,6 +79,35 @@ app = FastAPI(
     description="Backend API for Civic Complaint Management System",
     version="1.0"
 )
+# =========================================================
+# WEB3 / SEPOLIA CONFIGURATION
+# =========================================================
+
+RPC_URL = os.getenv("RPC_URL")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+CONTRACT_ADDRESS = os.getenv("CONTRACT_ADDRESS")
+
+web3 = None
+contract = None
+account = None
+
+if RPC_URL and PRIVATE_KEY and CONTRACT_ADDRESS:
+
+    web3 = Web3(Web3.HTTPProvider(RPC_URL))
+
+    account = web3.eth.account.from_key(PRIVATE_KEY)
+
+    CONTRACT_ADDRESS = Web3.to_checksum_address(
+        CONTRACT_ADDRESS
+    )
+
+    with open("CivicProof_abi.json", "r") as f:
+        CONTRACT_ABI = json.load(f)
+
+    contract = web3.eth.contract(
+        address=CONTRACT_ADDRESS,
+        abi=CONTRACT_ABI
+    )
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 app.add_middleware(
