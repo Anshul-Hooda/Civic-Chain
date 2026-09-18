dbfbb16580d16fbdfcb457f4fedf76b28dfd00db
Anishka <anishkabhardwaj1608@gmail.com>
2026-09-18 20:41:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -224,7 +224,7 @@ function formatComplaintId(id) {
     const number = Number(id);
 
     return Number.isFinite(number)
-        ? `CC-${String(number).padStart(6, "0")}`
+        ? `CF-${String(number).padStart(6, "0")}`
         : String(id || "UNKNOWN");
 }
 
