5bb3457c72c0e20d52c2d32ad6fd162bd9155186
JiyaKhatri <jiyagkhatri@gmail.com>
2026-09-18 23:49:19 +0530

    Fix city department initialization

--- script.js
+++ script.js
@@ -45,8 +45,7 @@ const CITY_CENTERS = {
     gurgaon: [28.4595, 77.0266],
     rohtak: [28.8955, 76.6066]
 };
-
- const CITY_AREAS = {
+const CITY_AREAS = {
     delhi: [
         "Rohini",
         "Pitampura",
@@ -74,13 +73,45 @@ const CITY_CENTERS = {
         "Mehrauli",
         "Vasant Kunj",
         "Others"
-    ]
-};      
+    ],
 
-   
-    
-    
-        
+    "new delhi": [
+        "Connaught Place / New Delhi",
+        "Civil Lines",
+        "Karol Bagh",
+        "Others"
+    ],
+
+    sonipat: [
+        "Sonipat",
+        "Murthal",
+        "Gohana",
+        "Kundli",
+        "Others"
+    ],
+
+    gurugram: [
+        "Gurugram",
+        "DLF Phase 1",
+        "DLF Phase 2",
+        "DLF Phase 3",
+        "Sohna Road",
+        "Golf Course Road",
+        "Manesar",
+        "Others"
+    ],
+
+    gurgaon: [
+        "Gurugram",
+        "DLF Phase 1",
+        "DLF Phase 2",
+        "DLF Phase 3",
+        "Sohna Road",
+        "Golf Course Road",
+        "Manesar",
+        "Others"
+    ]
+};
 
 let allComplaints = [];
 let blockchainHashesByComplaintId = new Map();
@@ -12131,7 +12162,7 @@ async function initializeCityfile() {
     initializeMapFilters();
 
     initializeComplaintForm();
-    initializeAreaSelect();
+
     initializeCityDepartmentLink();
 
     initializeEvidencePreview();
